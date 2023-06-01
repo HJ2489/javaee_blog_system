5abce6fbf520151d6955842d4e37862cdf3759fc
HJ2489 <2489436188@qq.com>
2023-06-01 16:52:40 +0800

    2023-06-01 16:51:30 first commit

---
+++ src/main/java/com/torch/JavaeeBlogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.torch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaeeBlogSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaeeBlogSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/torch/JavaeeBlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaeeBlogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
